3436cc7a51f92f3d3e73510b312f9c6e7e8f5b3b
Ayende Rahien <Ayende@ayende.com>
2020-06-12 02:02:01 +0300

    Implementing allocators

--- src/main.c
+++ src/main.c
@@ -5,181 +5,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <assert.h>
 #include <errno.h>
 
-struct selected_range {
-  size_t position;
-  size_t size_available;
-};
+int main()
+{
 
-struct range_finder {
-  // input
-  uint64_t *bitmap;
-  size_t bitmap_size;
-  size_t size_required;
-  size_t index;
+  // uint64_t x[] = {0x80520A27102E21, 0xE0000E00020, 0x1A40027F025802C8,
+  //                 0xEE6ACAE56C6C3DCC};
 
-  // output
-  struct selected_range selection;
-
-  // the current word we are working on
-  uint64_t current;
-
-  // state
-  uint64_t current_set_bit;
-  uint64_t previous_set_bit;
-};
-
-static bool handle_zero_word(struct range_finder *restrict range) {
-  range->current_set_bit = (range->index + 1) * 64;
-  if (range->current_set_bit > range->previous_set_bit + range->size_required) {
-    range->selection.position =
-        range->previous_set_bit + 1; // intentionally overflowing here
-    range->selection.size_available =
-        (range->current_set_bit - range->selection.position);
-    return true;
-  }
-  return false;
-}
-
-static bool find_range_once(struct range_finder *restrict range) {
-  uint64_t bitset = range->current;
-
-  if (bitset == ULONG_MAX) {
-    // all bits are set, can skip whole thing
-    range->previous_set_bit = (range->index + 1) * 64 - 1;
-    return false;
-  }
-
-  if (bitset == 0) {
-    return handle_zero_word(range);
-  }
-
-  while (bitset != 0) {
-    int r = __builtin_ctzl(bitset);
-    range->current_set_bit = (range->index) * 64 + (uint64_t)r;
-    if (range->current_set_bit >
-        range->previous_set_bit + range->size_required) {
-      // intentionally overflowing here
-      range->selection.position = range->previous_set_bit + 1;
-      range->selection.size_available =
-          (range->current_set_bit - range->selection.position);
-      range->previous_set_bit = range->current_set_bit;
-      return true;
-    }
-    range->previous_set_bit = range->current_set_bit;
-    bitset ^= (bitset & -bitset);
-  }
-
-  return handle_zero_word(range);
-}
-
-static void init_range(uint64_t *bitmap, size_t bitmap_size,
-                       size_t size_required,
-                       struct range_finder *restrict range) {
-  range->bitmap = bitmap;
-  range->bitmap_size = bitmap_size;
-  range->size_required = size_required;
-  range->current = bitmap[0];
-  range->previous_set_bit = ULONG_MAX;
-  range->index = 0;
-}
-
-static bool find_next(struct range_finder *restrict range) {
-  do {
-    if (find_range_once(range)) {
-      if (range->current_set_bit % 64) {
-        // mask the already found item
-        uint64_t mask = ~(ULONG_MAX << (range->current_set_bit % 64));
-        range->current |= mask;
-      } else {
-        // run out in the current word, but maybe we
-        // have more in the next?
-        if (range->index + 1 < range->bitmap_size) {
-          range->index++;
-          range->current = range->bitmap[range->index];
-          continue;
-        } else {
-          range->current = ULONG_MAX;
-        }
-      }
-      return true;
-    }
-    range->index++;
-    if (range->index >= range->bitmap_size)
-      return false;
-    range->current = range->bitmap[range->index];
-  } while (true);
-}
-
-static bool find_smallest_nearby_buffer(struct range_finder *restrict range) {
-  struct selected_range current = {0, SIZE_MAX};
-  size_t pickiness_boundary =
-      (range->index + 1) * 64 +
-      // the bigger the request range, the less we care about locality
-      +range->size_required;
-
-  while (find_next(range)) {
-    if (range->size_required == range->selection.size_available)
-      return true;
-    if (current.size_available > range->selection.size_available) {
-      current = range->selection;
-    }
-    if (range->selection.position > pickiness_boundary) {
-      // We have gone too far? Stop being choosy
-      if (current.size_available < range->selection.size_available) {
-        range->selection = current;
-      }
-      return true;
-    }
-  }
-
-  range->selection = current;
-
-  return current.size_available != SIZE_MAX;
-}
-
-static bool find_best_buffer(uint64_t *bitmap, size_t bitmap_size,
-                             size_t size_required, size_t near_pos,
-                             size_t *bit_pos) {
-  if (!size_required || near_pos / 64 >= bitmap_size)
-    return false;
-
-  size_t high = near_pos / 64;
-
-  struct range_finder range;
-  init_range(bitmap + high, bitmap_size - high, size_required, &range);
-
-  if (find_smallest_nearby_buffer(&range)) {
-    *bit_pos = range.selection.position + high * 64;
-    return true;
-  }
-  if (!high) {
-    return false; // already scanned it all
-  }
-
-  // we search _high_, couldn't find anything, maybe lower?
-  init_range(bitmap, high, size_required, &range);
-  if (find_next(&range)) {
-    *bit_pos = range.selection.position;
-    return true;
-  }
-
-  return false;
-}
-
-int main() {
-
-  uint64_t x[] = {0x80520A27102E21, 0xE0000E00020, 0x1A40027F025802C8,
-                  0xEE6ACAE56C6C3DCC};
-
-  size_t pos;
-  if (find_best_buffer(x, 4, 1, 12, &pos)) {
-    printf("%zu\n", pos);
-  } else {
-    printf("none\n");
-  }
   //   struct range_finder range;
   //   init_range(x, 4, 1, &range);
 
@@ -187,6 +21,29 @@ int main() {
   //     printf("%zu (%zu)\n", range.selection.position,
   //            range.selection.size_available);
   //   }
+
+  database_t db;
+  database_options_t options = {0};
+  txn_t tx;
+  page_t page;
+
+  assert(open_database("db/orev", &options, &db));
+
+  assert(create_transaction(&db, 0, &tx));
+  assert(allocate_page(&tx, &page));
+
+  printf("New allocated page %lu\n", page.page_num);
+  strcpy(page.address, "Hello Gavran");
+
+  assert(commit_transaction(&tx));
+  assert(close_transaction(&tx));
+  assert(create_transaction(&db, 0, &tx));
+  assert(get_page(&tx, &page));
+
+  printf("%s\n", page.address);
+
+  assert(close_transaction(&tx));
+  assert(close_database(&db));
 
   return 0;
 }
--- src/transactions.h
+++ src/transactions.h
@@ -1,55 +1,67 @@
 #pragma once
-#include <stdint.h>
-#include <stdbool.h>
-#include <stddef.h>
 #include "errors.h"
 #include "pal.h"
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 
 #define PAGE_SIZE 8192
 #define PAGE_ALIGNMENT 4096
 
-_Static_assert(PAGE_SIZE && PAGE_SIZE % PAGE_ALIGNMENT == 0, "PAGE_SIZE must be of a multiple of 4096");
+_Static_assert(PAGE_SIZE &&PAGE_SIZE % PAGE_ALIGNMENT == 0,
+               "PAGE_SIZE must be of a multiple of 4096");
 
-typedef struct page{
-    uint64_t page_num;
-	void* address;
+typedef struct page
+{
+  uint64_t page_num;
+  void *address;
 } page_t;
 
 typedef struct transaction_state txn_state_t;
 
-typedef struct transaction {
-    txn_state_t* state;
+typedef struct transaction
+{
+  txn_state_t *state;
 } txn_t;
 
 #define FILE_HEADER_MAGIC (9410039042695495) // = 'Gavran!\0'
 
-typedef struct file_header {
-    uint64_t magic;
-    uint64_t number_of_pages;
-    uint32_t version;
-    uint32_t page_size;
-
+typedef struct file_header
+{
+  uint64_t magic;
+  uint64_t number_of_pages;
+  uint64_t free_space_bitmap_start;
+  uint64_t free_space_bitmap_in_pages;
+  uint32_t version;
+  uint32_t page_size;
 } file_header_t;
 
-typedef struct database_options{
-    uint64_t minimum_size;
+typedef struct database_options
+{
+  uint64_t minimum_size;
 } database_options_t;
 
 typedef struct database_state db_state_t;
 
-typedef struct database {
-    db_state_t* state;
+typedef struct database
+{
+  db_state_t *state;
 } database_t;
 
-MUST_CHECK bool open_database(const char* path, database_options_t* options, database_t* db);
-MUST_CHECK bool close_database(database_t* db);
+MUST_CHECK bool open_database(const char *path, database_options_t *options,
+                              database_t *db);
+MUST_CHECK bool close_database(database_t *db);
 
-MUST_CHECK bool create_transaction(database_t* db, uint32_t flags, txn_t* tx);
+MUST_CHECK bool create_transaction(database_t *db, uint32_t flags, txn_t *tx);
 
-MUST_CHECK bool get_page(txn_t* tx, page_t* page);
-MUST_CHECK bool modify_page(txn_t* tx, page_t* page);
-MUST_CHECK bool allocate_page(txn_t tx, page_t* page);
-MUST_CHECK bool free_page(txn_t* tx, page_t* page);
+MUST_CHECK bool get_page(txn_t *tx, page_t *page);
+MUST_CHECK bool modify_page(txn_t *tx, page_t *page);
+MUST_CHECK bool allocate_page(txn_t *tx, page_t *page);
+MUST_CHECK bool free_page(txn_t *tx, page_t *page);
 
-MUST_CHECK bool commit_transaction(txn_t* tx);
-MUST_CHECK bool close_transaction(txn_t* tx);
+MUST_CHECK bool commit_transaction(txn_t *tx);
+MUST_CHECK bool close_transaction(txn_t *tx);
+
+bool find_free_range_in_bitmap(uint64_t *bitmap, size_t bitmap_size,
+                               size_t size_required, size_t near_pos,
+                               size_t *bit_pos);
