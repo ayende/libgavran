c6b721fc999661004f9189800c85fde6a88e704e
Ayende Rahien <Ayende@ayende.com>
2020-08-20 01:13:04 +0300

    working on ch16

--- ch16/code/btree.c
+++ ch16/code/btree.c
@@ -16,36 +16,33 @@ result_t btree_create(txn_t* tx, uint64_t* tree_id) {
   return success();
 }
 
+// tag::btree_search_pos_in_page[]
 ssize_t btree_search_pos_in_page(
-    page_t* p, page_metadata_t* metadata, btree_val_t* kvp) {
+    page_t* p, page_metadata_t* metadata, span_t* key) {
   uint16_t max_pos = metadata->tree.floor / sizeof(uint16_t);
   size_t high = max_pos - 1, low = 0;
   uint16_t* positions = p->address;
   int last_match      = 0;
   size_t last_pos     = 0;
   while (low <= high) {
-    last_pos = (low + high) / 2;
-    uint64_t key_size;
-    char* key =
-        varint_decode(p->address + positions[last_pos], &key_size);
-    last_match =
-        memcmp(kvp->key.address, key, MIN(kvp->key.size, key_size));
-    if (last_match == 0) break;
+    last_pos = (low + high) >> 1;
+    uint64_t ks;
+    char* cur  = varint_decode(p->address + positions[last_pos], &ks);
+    last_match = memcmp(key->address, cur, MIN(key->size, ks));
+    if (last_match == 0) return last_pos;
     if (last_match < 0) {
       low = last_pos + 1;
     } else {
       high = last_pos - 1;
     }
   }
-  if (last_match < 0) {
-    last_pos++;  // last pos is smaller than key, shift it
-  }
-  if (last_match) {
-    last_pos = ~last_pos;  // not an exact match
-  }
-  return last_pos;
+  // if last pos is smaller than key, shift it to where it should go
+  if (last_match < 0) last_pos++;
+  return ~last_pos;  // not an exact match
 }
+// end::btree_search_pos_in_page[]
 
+// tag::btree_set_in_page[]
 void btree_set_in_page(txn_t* tx, page_t* p,
     page_metadata_t* metadata, btree_val_t* set, btree_val_t* old) {
   ssize_t pos         = btree_search_pos_in_page(p, metadata, set);
@@ -53,17 +50,14 @@ void btree_set_in_page(txn_t* tx, page_t* p,
   size_t total_size   = varint_get_length(set->key.size) +
                       set->key.size + varint_get_length(set->val);
   if (pos >= 0) {
-    uint64_t ks, v;
+    uint64_t key_size, v;
     char* start   = p->address + positions[pos];
-    char* key_end = varint_decode(start, &ks) + ks;
+    char* key_end = varint_decode(start, &key_size) + key_size;
     char* end     = varint_decode(key_end, &v);
     if (old) {
       old->has_val = true;
       old->val     = v;
-    } else {
-      old->has_val = false;
     }
-
     if (total_size <= (end - start)) {  // can fit old location
       char* val_end = varint_encode(set->val, key_end);
       memset(val_end, 0, end - val_end);
@@ -72,6 +66,7 @@ void btree_set_in_page(txn_t* tx, page_t* p,
     }
     memset(start, 0, end - start);  // reset value
   } else {
+    if (old) old->has_val = false;
     pos            = ~pos;
     size_t max_pos = metadata->tree.floor / sizeof(uint16_t);
     metadata->tree.floor += sizeof(uint16_t);
@@ -88,3 +83,4 @@ void btree_set_in_page(txn_t* tx, page_t* p,
   positions[pos] = metadata->tree.ceiling;
   metadata->tree.free_space -= total_size;
 }
+// end::btree_set_in_page[]
